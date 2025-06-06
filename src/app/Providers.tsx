dede7e7c0a203a88da3ebf56b8cb1dc5ec2d156f
Contzo <ilierazvan1497@outloook.com>
2025-06-06 18:45:39 +0300

    tried to fix

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -5,43 +5,22 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { RainbowKitProvider, lightTheme } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  anvil,
-  zksync,
-  sepolia,
-} from "wagmi/chains";
+import config from "@/rainbowKitConfig";
 
-const config = getDefaultConfig({
-  appName: "TSender",
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-  chains: [mainnet, optimism, arbitrum, base, zksync, sepolia, anvil],
-  ssr: false,
-});
 
-export function Providers({ children }: { children: ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
-  const [hasMounted, setHasMounted] = useState(false);
-
-  // Prevent hydration issues by deferring RainbowKitProvider rendering
-  useEffect(() => {
-    setHasMounted(true);
-  }, []);
-
-  if (!hasMounted) return null; // Or a fallback/loading component
-
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider theme={lightTheme({ borderRadius: "medium" })}>
-          {children}
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
-}
+export  function Providers({ children }) {
+    const [queryClient] = useState(() => new QueryClient())
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => setMounted(true), []);
+    if(!mounted) return false 
+    return (
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>
+            {children}
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    );
+  }
 
